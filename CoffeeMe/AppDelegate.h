9cbcebeac820720b15be1ebb3d01478b5ce5d9a9
Mark Feaver <mark.feaver@gmail.com>
2015-06-13 18:09:32 +1200

    Created the initial Xcode project

---
+++ CoffeeMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeMe
+//
+//  Created by Mark Feaver on 13/06/15.
+//  Copyright (c) 2015 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeMe
+//
+//  Created by Mark Feaver on 13/06/15.
+//  Copyright (c) 2015 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
