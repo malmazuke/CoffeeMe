d1a69a24e2406e3b811e920ecb959d70f272cd73
Mark Feaver <mark.feaver@gmail.com>
2015-06-13 20:50:01 +1200

    Begin creating a Login VC

---
+++ CoffeeMe/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CoffeeMe
+//
+//  Created by Mark Feaver on 13/06/15.
+//  Copyright (c) 2015 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
