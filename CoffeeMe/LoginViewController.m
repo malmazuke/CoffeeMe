e82430d31df7fbc5855e4608745e78b948bd06ff
Mark Feaver <mark.feaver@gmail.com>
2015-06-15 18:18:17 +1200

    Login VC is now dismissed after successful login

--- CoffeeMe/LoginViewController.m
+++ CoffeeMe/LoginViewController.m
@@ -17,16 +17,6 @@
 
 @implementation LoginViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (IBAction)tappedLogin:(UIButton *)sender {
     [self mgf_loginWithFacebook];
 }
@@ -47,19 +37,9 @@
                 NSLog(@"Did log in with permissions: %@", result.grantedPermissions);
             }
             // TODO: Notify AWS Cognito of log in/creds
-            // TODO: Proceed to dashboard
+            [self dismissViewControllerAnimated:YES completion:nil];
         }
     }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
