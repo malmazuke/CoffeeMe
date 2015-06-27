76d8e3565014478cd69d6bc4c20ea8ccafb325be
Mark Feaver <mark.feaver@gmail.com>
2015-06-27 14:42:24 +1200

    Changed imports to use modules

--- CoffeeMe/APIClient.h
+++ CoffeeMe/APIClient.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Mark Feaver. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @class User;
 @class UserDescriptor;
--- CoffeeMe/User.h
+++ CoffeeMe/User.h
@@ -6,7 +6,8 @@
 //  Copyright (c) 2015 Mark Feaver. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+
 #import <Mantle/Mantle.h>
 
 typedef NS_ENUM(NSUInteger, UserGender) {
